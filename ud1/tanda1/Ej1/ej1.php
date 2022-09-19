235b4b890469ef1d5bb85a66ce7351dff3ac48f3
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-09-19 09:52:43 +0200

    tanda 1 hasta el 5

--- ud1/tanda1/Ej1/ej1.php
+++ ud1/tanda1/Ej1/ej1.php
@@ -8,7 +8,25 @@
 </head>
 <body>
     <?php
-        echo date("d\\t\h F Y, l");
+        echo date("d\\t\h F Y, l")."<br>";
+
+        $arrPalabras= array("Hola", "que", "tal");
+        $txt = implode(" ", $arrPalabras);
+        echo $txt."<br>";
+
+        echo str_replace("ñ", "gn", "ñoñeria")."<br>";
+
+        function generadorNum($n, $limite1, $limite2){
+            for ($i=0; $i < $n; $i++) { 
+                echo rand($limite1, $limite2)." -- ";
+            }
+        }
+
+        generadorNum(5, 4, 40);
+        echo "<br>"
+
+        // queda 2 y ultimo
+
     ?>
 </body>
 </html>
