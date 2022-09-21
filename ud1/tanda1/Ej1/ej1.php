533d2ab9c3b9a0fe55e0d2beb7e1ece8b1550719
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-09-21 13:24:09 +0200

    tanda1

--- ud1/tanda1/Ej1/ej1.php
+++ ud1/tanda1/Ej1/ej1.php
@@ -8,7 +8,12 @@
 </head>
 <body>
     <?php
+
         echo date("d\\t\h F Y, l")."<br>";
+
+        $segParaTerminarAnio = mktime (0,0,0,1,1,2023) - time();
+        $diasParaTerminarAnio = floor($segParaTerminarAnio / (60*60*24));
+        echo "quedan ".$diasParaTerminarAnio." dias para que termine el año";
 
         $arrPalabras= array("Hola", "que", "tal");
         $txt = implode(" ", $arrPalabras);
@@ -17,15 +22,25 @@
         echo str_replace("ñ", "gn", "ñoñeria")."<br>";
 
         function generadorNum($n, $limite1, $limite2){
+            $txt = "";
             for ($i=0; $i < $n; $i++) { 
-                echo rand($limite1, $limite2)." -- ";
+                $txt .= rand($limite1, $limite2)." -- ";
             }
+            return $txt;
+        }
+        
+
+      
+        echo generadorNum(5, 4, 40)."<br>";
+
+        
+        function cifrar($cadena){
+            $cifrado = ["A" => "20", "H"=>"9R", "M"=>"abcd"];
+            $cadena = strtoupper($cadena);
+            return str_replace(array_keys($cifrado), array_values($cifrado), $cadena);
         }
 
-        generadorNum(5, 4, 40);
-        echo "<br>"
-
-        // queda 2 y ultimo
+        echo cifrar("Hola amo");
 
     ?>
 </body>
