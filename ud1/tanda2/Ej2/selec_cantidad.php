b6c1ee0f7beee338cc4b88da3837a7c9dc0da90e
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-09-28 13:16:49 +0200

    /

---
+++ ud1/tanda2/Ej2/selec_cantidad.php
@@ -0,0 +1,22 @@
+<?php
+    function contarImg (){
+        
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cantidad</title>
+</head>
+<body>
+    <label for="selCantidad"></label>
+    <select name="selCantidad" id="selCantidad">
+        <?php
+
+        ?>
+    </select>
+</body>
+</html>
