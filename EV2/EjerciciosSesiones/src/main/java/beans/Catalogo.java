4a493ab5eb2402c4cfe645f3d5ee6e01a5937bde
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-12-13 10:10:40 +0100

    /

--- EV2/EjerciciosSesiones/src/main/java/beans/Catalogo.java
+++ EV2/EjerciciosSesiones/src/main/java/beans/Catalogo.java
@@ -1,7 +1,7 @@
 package beans;
 
 public class Catalogo {
-	static String [] libros = {"El imperio final", "El pozo de la ascension", "El heroe de las eras", "Aleacion de ley", "Sombras de identidad", "Brazales de duelo", "El metal perdido"};
+	private static String [] libros = {"El imperio final", "El pozo de la ascension", "El heroe de las eras", "Aleacion de ley", "Sombras de identidad", "Brazales de duelo", "El metal perdido"};
 	
 	
 	
